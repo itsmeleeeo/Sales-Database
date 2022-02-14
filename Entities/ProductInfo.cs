398c87257a743be9640b62352a5ca60dc0f39468
Leonardo Ferreira <leeeo.ferreira@hotmail.com>
2022-02-13 17:22:48 -0800

    criado o grid header e tentei popular, porem com erro vou corrigir

---
+++ Entities/ProductInfo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1_lfe_gfr_41_82.Entities
+{
+    class ProductInfo
+    {
+        public string productCategory { get; set; }
+        public string productSubCategory { get; set; }
+        public string productName { get; set; }
+        public int orderQuantity { get; set; }
+        public double sales { get; set; }
+        public double unitPrice { get; set; }
+        public string shippingMode { get; set; }
+        public double profit { get; set; }
+        public string customerName { get; set; }
+        public string customerSegment { get; set; }
+        public string province { get; set; }
+
+        public ProductInfo(string nProductCategory, string nProductSubCategory, string nProductName, int nOrderQuantity, double nSales, double nUnitPrice, 
+            string nShippingMode, double nProfit, string nCustomerName, string nCustomerSegnment, string nProvince)
+        {
+            this.productCategory = nProductCategory;
+            this.productSubCategory = nProductSubCategory;
+            this.productName = nProductName;
+            this.orderQuantity = nOrderQuantity;
+            this.sales = nSales;
+            this.unitPrice = nUnitPrice;
+            this.shippingMode = nShippingMode;
+            this.profit = nProfit;
+            this.customerName = nCustomerName;
+            this.customerSegment = nCustomerSegnment;
+            this.province = nProvince;
+        }
+    }
+}
---
+++ Utilities/ProductInfoParser.cs
@@ -0,0 +1,56 @@
+﻿using Assignment1_lfe_gfr_41_82.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Threading.Tasks;
+
+namespace Assignment1_lfe_gfr_41_82.Utilities
+{
+    class ProductInfoParser
+    {
+        public static List<ProductInfo> storage = new List<ProductInfo>();
+
+        public static List<ProductInfo> ParseRoster(string fileContents)
+        {
+            string[] lines = fileContents.Split('\n');
+
+            foreach(string line in lines)
+            {
+                //string[] fields = line.Trim().Split('\u002C');
+                string[] fields = line.Trim().Split('|');
+
+                if(fields.Length != 11)
+                {
+                    MessageBox.Show("Problem parsing file, check format");
+                    break;
+                } else {
+                    try
+                    {
+                        string[] productFields = fields[1].Split('\\');
+                        fields[1] = productFields[0];
+
+                        ProductInfo np = new ProductInfo(
+                            fields[0].Trim(),
+                            fields[1].Trim(),
+                            fields[2].Trim(),
+                            Convert.ToInt32(fields[3]),
+                            Convert.ToDouble(fields[4]),
+                            Convert.ToDouble(fields[5]),
+                            fields[6].Trim(),
+                            Convert.ToDouble(fields[7].Trim()),
+                            fields[8].Trim(),
+                            fields[9].Trim(),
+                            fields[10].Trim());
+                        storage.Add(np);
+                    } catch(Exception ex)
+                    {
+                        Console.WriteLine($"Error {ex}");
+                    }
+                }
+            }
+            return storage;
+        }
+    }
+}
